42d5a364465edc18508e09752015eb36a722ba43
vic-74 <victormirandafernandes574@gmail.com>
2025-04-11 09:23:46 -0300

    autoalização e arquivos

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-unction atualizarRelogio() {
-    const relogio = document.getElementById('relogio')
-    //Aqui eu vou mexer no elemento com ID 'relógio' no HTML
-    const agora = new Date();
-    agora.setHours(agora.getHours()-3)
-    //Essa linha serve para definir para o fuso horário correto
-    const horas = String(agora.getHours()).padStart(2,'0');
-    const minutos = String(agora.getMinutes()).padStart(2,'0');
-    const segundos = String(agora.getSeconds()).padStart(2,'0');
-    relogioElement.textContent = `${horas}:${minutos}:${segundos} `
-}
+function hora(){
+    //relogio receba a data atual
+    let tempo = new Date();
+   let relogio = Document.getElementByid('relogio')
+   let horas = tempo.getHours();
+   let minutos = tempo.getMinutos();
+   let segundos = tempo.getSeconds();
+
+   relogio.innerHTML
+
+  }
+  
+  hora();
