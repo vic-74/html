02ab3f2394fad32774653fcb4871d909940ff069
vic-74 <victormirandafernandes574@gmail.com>
2025-04-15 07:59:49 -0300

    autolização

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function hora(){
-    //relogio receba a data atual
+  //relogio receba a data atual
 
-    let relogio = document.getElementById('relogio')
+  let relogio = document.getElementById('relogio')
 
 
-    let tempo = new Date();
-    let horas = tempo.getHours();
-   let minutos = tempo.getMinutes();
-   let segundos = tempo.getSeconds();
+  let tempo = new Date();
+  let horas = String(tempo.getHours()).padStart(2,'0');
+ let minutos = String(tempo.getMinutes()).padStart(2,'0');
+ let segundos = String(tempo.getSeconds()).padStart(2,'0');
 
 relogio.innerHTML = `${horas}:${minutos}:${segundos}`
 
-  }
-  
-  hora();
+}
+setInterval(hora,1000)  
